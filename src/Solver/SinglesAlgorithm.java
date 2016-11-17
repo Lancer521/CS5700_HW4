a829d92b84369a7b86d62b476725487be023cb8f
Lancer521 <tylen.smith21@gmail.com>
2016-11-16 23:54:00 -0700

    Added 6 algorithms with notes on what each algorithm does.  Adjusted SudokuAlgorithm superclass

---
+++ src/Solver/SinglesAlgorithm.java
@@ -0,0 +1,14 @@
+package Solver;
+
+/**
+ * Created by Ty on 11/16/2016.
+ */
+public class SinglesAlgorithm extends SudokuAlgorithm {
+
+    @Override
+    protected void applyMethod() {
+        //Search all rows
+        //Set value for singles
+        //Remove from other cells' possVals
+    }
+}
